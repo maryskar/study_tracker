cc5dcafc3b22509a3c73954d3b1e0baddc2717b4
Марина <marina-potapova04@mail.ru>
2025-04-04 19:56:02 +0300

    [+] Add get_users function

--- backend/app/routes/users.py
+++ backend/app/routes/users.py
@@ -1,19 +1,18 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.database.database import get_db
-from app.models.user import User
 from sqlalchemy.future import select
-
-from sqlalchemy.orm import Session
+from app.models.user import User
+from app.schemas.user import UserCreate, UserOut
 from app.crud.user import create_user
-from app.database.database import SessionLocal
-
 from app.database.database import get_db
 
 router = APIRouter()
 
-from app.schemas.user import UserCreate
+@router.get("/users/", response_model=list[UserOut])
+async def get_users(db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(User))
+    return result.scalars().all()
 
-@router.post("/users/")
-def create_user_route(user: UserCreate, db: Session = Depends(get_db)):
-    return create_user(db=db, username=user.username, hashed_password=user.hashed_password)
+@router.post("/users/", response_model=UserOut)
+async def create_user_route(user: UserCreate, db: AsyncSession = Depends(get_db)):
+    return await create_user(db=db, user=user)
