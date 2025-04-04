8b051d591ee684f2963901caa455763b0daf05e2
Марина <marina-potapova04@mail.ru>
2025-04-04 20:10:23 +0300

    [+] Add create tables when app start

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 from app.routes import users
+from app.database.database import create_tables
 
 app = FastAPI()
 
+# Подключение роутеров
 app.include_router(users.router)
+
+# Создание таблиц при запуске приложения
+@app.on_event("startup")
+async def startup():
+    await create_tables()
