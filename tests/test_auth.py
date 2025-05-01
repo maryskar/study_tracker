870a8ba70f0d9715883fbdda995d1934459e49e0
SuspiciousLight <117872667+SuspiciousLight@users.noreply.github.com>
2025-05-01 22:36:55 +0300

    add func setUp
     Чистим таблицу users

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,4 +1,12 @@
 import unittest
 from database import Database
 from auth import AuthManager
+from jose import jwt
+import datetime
 
+class TestAuthManager(unittest.TestCase):
+    def setUp(self):
+        self.db = Database()
+        self.db.conn.execute("DELETE FROM users")
+        self.db.conn.commit()
+        self.auth = AuthManager(self.db)
