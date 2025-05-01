766af91f8b67773f73d4f0ff485dab9e7b46bb32
SuspiciousLight <117872667+SuspiciousLight@users.noreply.github.com>
2025-05-01 22:38:44 +0300

    add func test_register_user_success and fix name
     Повторная регистрация тем же логином должна упасть

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -5,8 +5,13 @@ from jose import jwt
 import datetime
 
 class TestAuthManager(unittest.TestCase):
-    def setUp(self):
+    def setUpUser(self):
         self.db = Database()
         self.db.conn.execute("DELETE FROM users")
         self.db.conn.commit()
         self.auth = AuthManager(self.db)
+
+    def test_register_user_success(self):
+        result = self.auth.register("alice", "password123")
+        self.assertTrue(result)
+        self.assertFalse(self.auth.register("alice", "anotherpass"))
