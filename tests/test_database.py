411179e47e4cbc4ca387ae2f247e7d216fd83962
SuspiciousLight <117872667+SuspiciousLight@users.noreply.github.com>
2025-05-01 23:57:54 +0300

    Add setUp in test database

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,4 +1,10 @@
 import unittest
 from program_files.database import Database
+from datetime import datetime, timedelta
 
 class TestDatabase(unittest.TestCase):
+    def setUp(self):
+        self.db = Database()
+        for tbl in ("users", "study_sessions", "achievements"):
+            self.db.conn.execute(f"DELETE FROM {tbl}")
+        self.db.conn.commit()
