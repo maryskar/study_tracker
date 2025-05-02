822f78ed6d53e8f270726c9c5ab28ca675b97b9b
SuspiciousLight <117872667+SuspiciousLight@users.noreply.github.com>
2025-05-03 02:14:16 +0300

    update test_api_motivation

--- tests/test_api_motivation.py
+++ tests/test_api_motivation.py
@@ -3,13 +3,13 @@ from unittest.mock import patch
 from program_files.api_client import MotivationAPI
 
 class TestMotivationAPI(unittest.TestCase):
-    @patch("api_client.requests.get")
+    @patch("program_files.api_client.requests.get")
     def test_get_quote_success(self, mock_get):
         mock_resp = mock_get.return_value
         mock_resp.json.return_value = {"content": "Вперёд!"}
         self.assertEqual(MotivationAPI.get_quote(), "Вперёд!")
 
-    @patch("api_client.requests.get", side_effect=Exception("fail"))
+    @patch("program_files.api_client.requests.get", side_effect=Exception("fail"))
     def test_get_quote_fallback(self, _):
         quote = MotivationAPI.get_quote()
         self.assertTrue(isinstance(quote, str))
